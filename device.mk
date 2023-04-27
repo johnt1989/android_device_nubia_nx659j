f8b5b5988c53819b9fd30398fef65d99ed2cc7b6
johnt1989 <johnjohnt1989@gmail.com>
2023-04-27 10:15:07 -0700

    .

--- device.mk
+++ device.mk
@@ -15,10 +15,3 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     qcom_decrypt \
     qcom_decrypt_fbe
-
-# Dependencies
-TARGET_RECOVERY_DEVICE_MODULES += \
-    libion
-
-RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so
