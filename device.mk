b2e850f8cf0a0aed55ade48cb62d43aa57c9b574
johnt1989 <johnjohnt1989@gmail.com>
2023-05-09 05:19:50 -0700

    .

--- device.mk
+++ device.mk
@@ -3,11 +3,6 @@ LOCAL_PATH := device/nubia/NX659J
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# qcom decryption
-PRODUCT_PACKAGES += \
-    qcom_decrypt \
-    qcom_decrypt_fbe
-
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
