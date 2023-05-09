b9e1bf6a1f303214ecf113a6951fd4699aa1482e
johnt1989 <johnjohnt1989@gmail.com>
2023-05-08 23:05:06 -0700

    .

--- device.mk
+++ device.mk
@@ -12,3 +12,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
+
+# Vendor
+PRODUCT_COPY_FILES += \
+	device/nubia/NX659J/recovery/root/system/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
